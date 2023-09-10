ecab77325e223a1f90e26f42d17dcb753a09309c
Thalisson <91104465+Thalisson-both@users.noreply.github.com>
2023-09-10 16:30:53 -0300

    init
    Upload arquivos escritos durante o modulo 7 do curso da ebac

---
+++ main.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e) {
+    let formEvalido = false;
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensageSucesso = `Montante de : <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta  <b>${numeroContaBeneficiario.value}</b>`;
+
+    formEvalido = validaNome(nomeBeneficiario.value)
+    if (formEvalido) {
+        const containerMensagemSuesso = document.querySelector('.success-message');
+        containerMensagemSuesso.innerHTML = mensageSucesso;
+        containerMensagemSuesso.style.display = 'block';
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+    } else {
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    }
+})
+
+nomeBeneficiario.addEventListener('keyup', function(e){
+    console.log(e.target.value);
+    formEvalido = validaNome(e.target.value);
+
+    if (!formEvalido) {
+        nomeBeneficiario.classList.add('error')
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        nomeBeneficiario.classList.remove('error')
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
